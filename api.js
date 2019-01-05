674ea0a5ed066a9e05cd15a4d2d082829f82bd11
Steve Martin <steven_martin@me.com>
2019-01-05 19:43:18 +0000

    Updated to use Twitter-Headlines 2.0

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const cron = require('node-cron');
 const express = require('express');
 const TwitterHeadlines = require('twitter-headlines');
 
-const headlines = new TwitterHeadlines({
+const news = new TwitterHeadlines({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
@@ -13,19 +13,17 @@ const headlines = new TwitterHeadlines({
 });
 
 const app = express();
-headlines.collect();
+news.get();
 
 app.get('/', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  headlines.read().then((response) => {
-    res.send(response);
-  });
+  res.send(news.headlines);
 });
 
 
 cron.schedule('0,15,30,45 * * * *', () => {
-  headlines.collect();
+  news.get();
 });
 
 const PORT = process.env.PORT || 3000;
